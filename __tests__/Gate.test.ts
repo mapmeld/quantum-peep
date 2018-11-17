ae3d867a322e7f3fdc7863e91e72d1cd260fb70c
Nick Doiron <ndoiron@mapmeld.com>
2018-11-17 05:06:26 -0500

    more simple gates

--- __tests__/Gate.test.ts
+++ __tests__/Gate.test.ts
@@ -1,7 +1,46 @@
 import { Gates } from '../src';
-test('Gate X', () => {
+test('Gate X (one qubit)', () => {
   let xgate = Gates.X(1);
   expect(xgate.code('quil')).toBe('X 1');
   expect(xgate.code('qasm')).toBe('x q[1];');
   expect(xgate.code('q#')).toBe('X(1);');
+});
+
+test('Gate I/ID (different names, same output)', () => {
+  let igate = Gates.I(1);
+  let idgate = Gates.ID(1);
+  expect(igate.code('quil')).toBe('I 1');
+  expect(igate.code('qasm')).toBe('id q[1];');
+  expect(igate.code('q#')).toBe('I(1);');
+  expect(idgate.code('quil')).toBe('I 1');
+  expect(idgate.code('qasm')).toBe('id q[1];');
+  expect(idgate.code('q#')).toBe('I(1);');
+});
+
+test('Gate Dagger S', () => {
+  let sdg = Gates.SDAG(1);
+  expect(sdg.code('quil')).toBe('DAGGER S 1');
+  expect(sdg.code('qasm')).toBe('sdg q[1];');
+  //expect(sdg.code('q#')).toBe('DAGGER S(1);');
+});
+
+test('Gate T and add DAGGER later', () => {
+  let tdg = Gates.T(1);
+  tdg.dagger();
+  expect(tdg.code('quil')).toBe('DAGGER T 1');
+  expect(tdg.code('qasm')).toBe('tdg q[1];');
+  //expect(tdg.code('q#')).toBe('DAGGER S(1);');
+
+  // subsequent adds don't do anything (should this throw an error?)
+  tdg.dagger();
+  expect(tdg.code('quil')).toBe('DAGGER T 1');
+
+});
+
+test('DAGGER X only supported by some platforms', () => {
+  let x = Gates.X(1);
+  x.dagger();
+  expect(x.code('quil')).toBe('DAGGER X 1');
+  expect(() => { x.code('qasm') }).toThrow(Error);
+  //expect(tdg.code('q#')).toBe('DAGGER X(1);');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,87 @@
 
 class BasicGate {
   name: string;
-  qubit: number;
-  static validGates: Array<string> = ['X'];
+  qubits: Array<number>;
+  static validGates: Array<string> = ['X', 'Y', 'Z', 'H', 'S', 'T', 'DAGGER S', 'DAGGER T', 'I'];
 
-  constructor (name: string, qubit: number) {
+  constructor (name: string, qubits: Array<number>) {
     if (BasicGate.validGates.indexOf(name.toUpperCase()) === -1) {
       throw new Error('Gate type unknown');
     }
-    this.name = name;
-    this.qubit = qubit;
+    this.name = name.toUpperCase();
+    this.qubits = qubits;
+  }
+
+  qasmVersion (quil_name: string) {
+    quil_name = quil_name.toLowerCase();
+    switch (quil_name) {
+      case 'i':
+        return 'id';
+      case 'dagger s':
+        return 'sdg';
+      case 'dagger t':
+        return 'tdg';
+    }
+    if (quil_name.indexOf('dagger') === 0) {
+      throw new Error('Dagger of gates other than S and T not supported for QASM');
+    } else {
+      return quil_name;
+    }
+  }
+
+  dagger () {
+    // available on Quil at least
+    if (this.name.indexOf('DAGGER') !== 0) {
+      this.name = 'DAGGER ' + this.name;
+    }
   }
 
   code (language: string) {
     if (language === 'quil') {
-      return `${this.name.toUpperCase()} ${this.qubit}`;
+      return `${this.name} ${this.qubits.join(' ')}`;
     } else if (language === 'q#') {
-      return `${this.name.toUpperCase()}(${this.qubit});`;
+      return `${this.name}(${this.qubits.join(' ')});`;
     } else if (language === 'qasm') {
-      return `${this.name.toLowerCase()} q[${this.qubit}];`;
+      return `${this.qasmVersion(this.name)} q[${this.qubits.join(' ')}];`;
     }
   }
 }
 
 export const Gates = {
   X: (qubit: number) => {
-    return new BasicGate('X', qubit);
+    return new BasicGate('X', [qubit]);
+  },
+  Y: (qubit: number) => {
+    return new BasicGate('Y', [qubit]);
+  },
+  Z: (qubit: number) => {
+    return new BasicGate('Z', [qubit]);
+  },
+  H: (qubit: number) => {
+    return new BasicGate('H', [qubit]);
+  },
+  S: (qubit: number) => {
+    return new BasicGate('S', [qubit]);
+  },
+  T: (qubit: number) => {
+    return new BasicGate('T', [qubit]);
+  },
+  SDAG: (qubit: number) => {
+    return new BasicGate('DAGGER S', [qubit]);
+  },
+  TDAG: (qubit: number) => {
+    return new BasicGate('DAGGER T', [qubit]);
+  },
+
+  // aliases for same thing
+  I: (qubit: number) => {
+    return new BasicGate('I', [qubit]);
+  },
+  ID: (qubit: number) => {
+    return new BasicGate('I', [qubit]);
   }
+
+  // CX: (qa: number, qb: number) => {
+  //   return new BasicGate('CX', [qa, qb]);
+  // }
 };
